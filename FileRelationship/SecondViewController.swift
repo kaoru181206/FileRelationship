716ed6ccffdaf46b605749916f09d80dd38ac12f
kaoru.shiraga <karats24.kaoru@gmail.com>
2022-05-13 11:51:08 +0900

    storyboardファイルとプログラムファイルの関係性チュートリアル

---
+++ FileRelationship/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  FileRelationship
+//
+//  Created by 白髪馨 on 2022/05/13.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBOutlet weak var label2: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        label2.text = "２つ目の画面"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
